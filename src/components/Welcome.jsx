d3e35e8f266dcdfa4e20d7c61015dd63cee12475
khoatran <khoa.tran@stanyangroup.com>
2024-07-02 14:09:36 +0700

    Add GSap Animation

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,66 +1,36 @@
 import React from 'react';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP);
 
 function Welcome() {
+    const container = useRef();
+    const circle = useRef();
+
+    useGSAP(
+      () => {
+        // use selectors...
+        gsap.to(".box", { x: "+=200", rotation: 360, duration: 3 });
+
+        // or refs...
+        // gsap.to(circle.current, { rotation: "-=360", duration: 3 });
+      },
+      { scope: container }
+    ); // <-- scope for selector text (optional)
+
     return (
-        <div className="relative isolate px-6 lg:px-8">
-            <div
-                className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-                aria-hidden="true"
-            >
-                <div
-                    className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                    style={{
-                        clipPath:
-                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                    }}
-                />
-            </div>
-            <div className="mx-auto max-w-2xl pt-12 sm:pt-24 lg:pt-36 pb-6">
-                x
-                <div className="text-center">
-                    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                        Build more. Break less. Empower others.
-                    </h1>
-                    <p className="mt-6 text-lg leading-8 text-gray-600">
-                        AJSON Schema enables the confident and reliable use of
-                        the JSON data format.
-                    </p>
-                    <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <button
-                            onClick={() =>
-                                window.scrollTo({
-                                    top: 400,
-                                    behavior: 'smooth',
-                                })
-                            }
-                            href="#"
-                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Get started
-                        </button>
-                        <a
-                            href="#"
-                            className="text-sm font-semibold leading-6 text-gray-900"
-                        >
-                            Learn more <span aria-hidden="true">→</span>
-                        </a>
-                    </div>
+        <div className="flex align-center justify-start min-h-screen w-full mx-auto mt-24 overflow-hidden">
+            <div ref={container} className="container">
+                <div className="box w-10 h-10 rounded-full bg-blue-700">selector</div>
+                {/* <div className="circle bg-gradient-to-r from-green-500 to-green-700" ref={circle}>
+                    Ref
+                </div> */}
                 </div>
-            </div>
-            <div
-                className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-                aria-hidden="true"
-            >
-                <div
-                    className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                    style={{
-                        clipPath:
-                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                    }}
-                />
-            </div>
+            {/* <div className="box bg-gradient-to-r from-blue-500 to-blue-700">selector</div> */}
         </div>
     );
-}
+  }
 
 export default Welcome;
